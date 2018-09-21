d5aa3f94a931015f876bb753aa3a2f8b3ac05e3c
Liz Khuu <khuu.liz@gmail.com>
2018-09-21 09:42:50 -0400

    Replace cartItems.includes() with inCart for efficiency in React

--- src/cart-button.js
+++ src/cart-button.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-let mapStateToProps = state => ({ cartItems: state.cartItems });
+let mapStateToProps = (state, props) => ({ inCart: state.cartItems.includes(props.product) });
 
 let CartButton = (props) => {
-    let button = props.cartItems.includes(props.product)
-        ? {type: 'REMOVE_FROM_CART', text: 'Remove from Cart'}
-        : {type: 'ADD_TO_CART', text: 'Add to Cart'}
+    let button = props.inCart
+    ? {type: 'REMOVE_FROM_CART', text: 'Remove from Cart'}
+    : {type: 'ADD_TO_CART', text: 'Add to Cart'}
     return <button
         onClick={() => {
             props.dispatch({ type: button.type, product: props.product })
